a1ac80a10bdd76883b546e3c67af1d1560a19047
Sean Shields <sean@dmgctrl.com>
2013-08-06 12:29:57 -0400

    performed brew upgrade, adding updated files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = '107e9b7bf24f7f5e068351ca55249ad4dca265ae6d5d431458d8d0e2d16bf411227d4b5cc3b0ef9809cd5b453959ca374973e8bdd7e864704080fee383999bf3'
+Treebook::Application.config.secret_key_base = '74bdb25b925371dea3427f05c3452d247a11ca37a2963766a2cfc8ad6c532eaec31457a50c5510b1f4b5b699803c2d11c00c561ae26f720ddf70381dae680499'
